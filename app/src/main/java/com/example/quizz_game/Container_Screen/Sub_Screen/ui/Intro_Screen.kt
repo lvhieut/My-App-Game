10770490c07e1e2ab4db23611878cd476963e9d4
HieuLv <levanhieu1566@gmail.com>
2024-03-23 09:59:37 +0700

    initialized commit

--- app/src/main/java/com/example/quizz_game/Container_Screen/Sub_Screen/ui/Intro_Screen.kt
+++ app/src/main/java/com/example/quizz_game/Container_Screen/Sub_Screen/ui/Intro_Screen.kt
@@ -2,6 +2,7 @@ package com.example.quizz_game.Container_Screen.Sub_Screen.ui
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -42,6 +43,32 @@ class Intro_Screen : Fragment() {
 
         }
 
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("hieulv","Pause FmA")
+    }
+
+    override fun onStop() {
+
+        super.onStop()
+        Log.d("hieulv","onStop FmA")
+    }
+
+    override fun onDestroyView() {
+        Log.d("hieulv","onDestroyView FmA")
+        super.onDestroyView()
+    }
+
+    override fun onDestroy() {
+        Log.d("hieulv","onDestroy FmA")
+        super.onDestroy()
+    }
+
+    override fun onDetach() {
+        Log.d("hieulv","onDetach FmA")
+        super.onDetach()
     }
 
 }
